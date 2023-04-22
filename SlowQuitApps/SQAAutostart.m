e716dab269a2668f31a98de41538863e225f2f4a
wark <warkcod@gmail.com>
2023-04-22 09:04:45 +0800

    Update SQAAutostart.m
    Support macOS Ventura, build with Xcode v14.3

--- SlowQuitApps/SQAAutostart.m
+++ SlowQuitApps/SQAAutostart.m
@@ -15,8 +15,34 @@ NSString * const LauncherBundleIdentifier = @"com.dteoh.SlowQuitAppsLauncher";
 }
 
 + (BOOL)shouldRegisterLoginItem:(BOOL)enabled {
-    return SMLoginItemSetEnabled((__bridge CFStringRef)(LauncherBundleIdentifier), enabled);
+    if(@available(macOS 13.0, *)) {
+        NSError *error = nil;
+        if(enabled) {
+            BOOL result = [SMAppService.mainAppService registerAndReturnError:&error];
+//            NSLog(@"Registration failed: %@", error.localizedDescription);
+            
+            NSAlert *warning = [[NSAlert alloc] init];
+            warning.alertStyle = NSAlertStyleWarning;
+            if (result) {
+                warning.messageText = NSLocalizedString(@"Success", nil);
+                warning.informativeText = error.localizedDescription;
+            } else {
+                warning.messageText = NSLocalizedString(@"Failed", nil);
+                warning.informativeText = error.localizedDescription;
+            }
+           
+            [warning addButtonWithTitle:NSLocalizedString(@"OK", nil)];
+            [warning runModal];
+            
+            return result;
+        } else {
+            return [SMAppService.mainAppService unregisterAndReturnError:&error];
+        }
+    } else {
+        return SMLoginItemSetEnabled((__bridge CFStringRef)(LauncherBundleIdentifier), enabled);
+    }
 }
+
 
 + (BOOL)enable {
     return [self shouldRegisterLoginItem:YES];
@@ -25,5 +51,6 @@ NSString * const LauncherBundleIdentifier = @"com.dteoh.SlowQuitAppsLauncher";
 + (BOOL)disable {
     return [self shouldRegisterLoginItem:NO];
 }
+
 
 @end
